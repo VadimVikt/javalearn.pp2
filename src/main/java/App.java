d26b43fe68c02d7f1bb690dce92be534685f4412
Vadim Bridnya <v-bridnya@yandex.ru>
2020-08-17 11:48:58 +0300

    task 2.1.2

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean.getMessage());
+
+        System.out.println(bean == bean1);
+
+        Cat beanCat = applicationContext.getBean(Cat.class);
+
+        Cat beanCat1 = applicationContext.getBean(Cat.class);
+
+        System.out.println(beanCat == beanCat1);
+    }
+}
